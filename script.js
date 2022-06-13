9e8cc04758b4a89fb82b80a6332c8e73cda250c8
Abdulrahman <bhabdulrahaman@gmail.com>
2022-06-13 08:47:21 +0100

    added a paragraph to my html file and styled in the style.css and that's it

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 window.addEventListener("keydown", function (e) {
-  const audio = this.document.querySelector(`audio[data-key= "${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key= "${e.keyCode}"]`);
   audio.currentTime = 0;
+  if (!audio) return;
+  audio.play();
 });
